0aaa19d5be9c8f9a7e249be7382f20fb7c0b6639
runemaker <runesmaker@yandex.ru>
2018-11-18 11:43:30 +0300

    variables

---
+++ var.py
@@ -0,0 +1,12 @@
+"""Rus. Присваеваем значение переменным. 
+Eng. Assign value to variables.
+"""
+varOne = "Hello"
+varTwo = "World"
+
+def pri(a,b):
+	print(a,b)
+	
+if __name__ == "__main__":
+	print(__doc__)
+	pri(varOne,varTwo)
